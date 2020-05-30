cfe539397017fe2088392f6cb05337a023a9f892
Jonathan Canela <jonathancanela365@gmail.com>
2020-05-30 10:35:17 -0700

    Finished MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+import { Row } from 'reactstrap'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +11,27 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then((response) => {
+      const characterData = response.data.results
+      setData(characterData)
+    })
+    .catch((err) => {
+      console.log('Error!', err)
+    })
+  }, [])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">
+        <Row>
+          {data.map((charData) => {
+           return <Character key={charData.id} data={charData} />
+          })}
+        </Row>
+      </h1>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react'
+import { Card, CardTitle, CardText, Container, Row, Col} from 'reactstrap'
+
+function Character(props){
+    const test = 'Test'
+    return(
+            <Col xs="6" sm="4" lg={{size: 'auto'}}>
+                <Card body>
+                    <CardTitle textSize="10">{props.data.name}</CardTitle>
+                    <CardText>{props.data.status}</CardText>
+                    <CardText>{props.data.species}</CardText>
+                </Card>
+            </Col>
+    )
+}
+
+export default Character;
